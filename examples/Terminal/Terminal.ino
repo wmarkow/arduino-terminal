b330ad18cffb4d287f066276303d6865cf197e2c
Witold Markowski <witold.a.markowski@gmail.com>
2016-05-24 23:25:26 +0200

    Fix example.

--- examples/Terminal/Terminal.ino
+++ examples/Terminal/Terminal.ino
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include <Array.h>
+#include <FixedSizeArray.h>
 #include <Terminal.h>
+#include <UptimeCommand.h>
 
-Terminal terminal;
+FixedSizeArray<AbstractCommand, 5> commands;
+Array<AbstractCommand> *array = &commands;
+UptimeCommand uptimeCommand;
+
+Terminal terminal(&Serial, array);
 
 void setup() {
 	Serial.begin(9600);
+
+	array->add(&uptimeCommand);
 }
 
 void loop() {
